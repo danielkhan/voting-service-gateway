c37246d43f375bd9adda642c2d6a03430724724d
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-13 23:40:03 +0200

    New Deployment

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const appsZipkin = require('appmetrics-zipkin');
-const appstatsd = require('appmetrics-statsd').StatsD({
-  global_tags: ['application=service-gateway']
-});
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
