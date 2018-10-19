37f4a2db44d7b6e46b54b46744801fdf4fadb511
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-19 10:02:28 -0300

    New Deployment

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,25 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const request = require('request');
-let toggle = false;
+let toggle = 0;
 
 router.get('/', (req, res, next) => {
-  if (!toggle) {
-
+  if (toggle % 3 !== 0) {
     request.get(`http://localhost:3010?choice=${req.query.choice}`, (e, r) => {
       if (e) return next(e);
       if(r.statusCode > 299) {
         return res.status(r.statusCode).json({error: 'Something went wrong!'});
       }
+      toggle++;
       return res.json(JSON.parse(r.body));
     });
-    toggle = true;
-    
   } else {
     request.get(`http://localhost:3020?choice=${req.query.choice}`, (e, r) => {
       if (e) return next(e);
       if(r.statusCode > 299) {
         return res.status(r.statusCode).json({error: 'Something went wrong!'});
       }
+      toggle++;
       return res.json(JSON.parse(r.body));
     });
   }
